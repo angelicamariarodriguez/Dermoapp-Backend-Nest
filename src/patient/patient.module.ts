c41ed72019200686fe7e07f664ed0f4e97f72c98
ivanbrij <ji.barrerar1@uniandes.edu.co>
2023-01-28 17:54:09 -0600

    Se agrega la logica de Paciente

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PatientService } from './patient.service';
+import { PatientEntity } from './patient.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([PatientEntity])],
+  providers: [PatientService]
+})
 export class PatientModule {}
