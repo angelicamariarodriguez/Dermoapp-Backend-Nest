91c6017b0af7f8c0f35aa85cce5a41e1c9edbf81
ivanbrij <ji.barrerar1@uniandes.edu.co>
2023-02-03 13:21:18 -0600

    Se configura Dockerfile para crear el contenedor del BE

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.listen(parseInt(process.env.PORT) || 3000)
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'api/v',
