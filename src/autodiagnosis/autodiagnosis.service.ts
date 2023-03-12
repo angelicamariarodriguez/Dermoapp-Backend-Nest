8eaa5138d4de3c67b8d3ad3624a983648303709a
ivanbrij <ji.barrerar1@uniandes.edu.co>
2023-03-12 00:29:14 -0600

    Se agregag prueba a controlador

--- src/autodiagnosis/autodiagnosis.service.ts
+++ src/autodiagnosis/autodiagnosis.service.ts
@@ -41,7 +41,13 @@ export class AutodiagnosisService {
       return await this.consultationService.update(consultationId, consultation);
 
     } catch (error) {
-      throw new BusinessLogicException("The consultation with the given id was not found or autodiagnosis unavailable", BusinessError.NOT_FOUND);//Error(e);
+      //throw new BusinessLogicException("The consultation with the given id was not found or autodiagnosis unavailable", BusinessError.NOT_FOUND);//Error(e);
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'The consultation with the given id was not found or autodiagnosis unavailable',
+      }, HttpStatus.NOT_FOUND, {
+        cause: error
+      });
 
     }
   }
